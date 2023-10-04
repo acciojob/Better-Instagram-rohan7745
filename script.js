542c6475d2d49a916831f32ddc56b0812e0ce7de
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2023-10-05 01:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function () {
+    let draggedDiv = null;
+
+    document.addEventListener("dragstart", function (event) {
+        if (event.target.classList.contains("draggable")) {
+            draggedDiv = event.target;
+        }
+    });
+
+    document.addEventListener("dragover", function (event) {
+        event.preventDefault();
+    });
+
+    document.addEventListener("drop", function (event) {
+        event.preventDefault();
+
+        if (draggedDiv && event.target.classList.contains("draggable")) {
+            const targetDiv = event.target;
+
+            // Swap the background images
+            const tempBackground = draggedDiv.style.backgroundImage;
+            draggedDiv.style.backgroundImage = targetDiv.style.backgroundImage;
+            targetDiv.style.backgroundImage = tempBackground;
+        }
+
+        draggedDiv = null;
+    });
+});
+
